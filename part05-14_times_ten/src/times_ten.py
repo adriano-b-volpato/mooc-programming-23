7b4290c189ab1f866c84fbd3180d84ab5fcb3b04
Adriano Buttigliero Volpato <adriano.b.volpato@gmail.com>
2024-01-23 19:57:24 -0300

    Exercise Times Ten done

--- part05-14_times_ten/src/times_ten.py
+++ part05-14_times_ten/src/times_ten.py
@@ -1,1 +1,12 @@
 # Write your solution here
+
+def times_ten(start_index: int, end_index: int):
+    new_dictionary = {}
+    for index in range(start_index, end_index+1):
+        new_dictionary[index] = index*10
+    return new_dictionary
+        
+        
+if __name__ == "__main__":
+    d = times_ten(3,6)
+    print(d)
