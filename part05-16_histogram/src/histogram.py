a9843a183ba548b726cb564ebe11eac80220e3f7
Adriano Buttigliero Volpato <adriano.b.volpato@gmail.com>
2024-01-24 11:09:38 -0300

    Histogram completed

--- part05-16_histogram/src/histogram.py
+++ part05-16_histogram/src/histogram.py
@@ -1,1 +1,24 @@
 # Write your solution here
+
+
+def histogram(string):
+    letters = {}
+    for letter in string:
+        if letter not in letters:
+            letters[letter] = 0
+        
+        letters[letter] += 1
+    for key, value in letters.items():
+        print(f"{key} {"*"*value}")
+    
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    histogram("statistically")
