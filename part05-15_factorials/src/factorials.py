29c77488fdab48537eb0c4495728159e9297632d
Adriano Buttigliero Volpato <adriano.b.volpato@gmail.com>
2024-01-23 23:27:56 -0300

    Exersice Factorials completed

--- part05-15_factorials/src/factorials.py
+++ part05-15_factorials/src/factorials.py
@@ -1,1 +1,17 @@
 # Write your solution here
+
+def factorials(n: int):
+    factorial_dictionary = {}
+    
+    for number in range(1, n+1):
+        factorial = number
+        for i in range(number-1, 1, -1):
+            factorial *= i
+        
+        factorial_dictionary[number] = factorial
+        
+        
+            
+    return factorial_dictionary
+        
+        
