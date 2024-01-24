99af6aa46246103fb5080a4e54fa18cda48417be
Adriano Buttigliero Volpato <adriano.b.volpato@gmail.com>
2024-01-24 11:32:55 -0300

    Phone Book V2

--- part05-18_phone_book_v2/src/phone_book_v2.py
+++ part05-18_phone_book_v2/src/phone_book_v2.py
@@ -1,1 +1,34 @@
 # Write your solution here
+
+
+phone_book = {"Adriano": [1166831828, 1166930174]}
+
+
+while True:
+    action = int(input("command (1 search, 2 add, 3 quit): "))
+    if action == 1:
+        name = input("name: ")
+        if name not in phone_book:
+            print("no number")
+            continue
+        for phone in phone_book[name]:
+            print(phone)
+        
+        
+        
+        
+    if action == 2:
+        name = input("name: ")
+        phone = input("number: ")
+        if name not in phone_book:
+            phone_book[name] = []
+        phone_book[name].append(phone)
+        print("ok!")
+        
+        
+        
+    if action == 3:
+        print("quitting...")
+        break
+    
+    
