1e6054f8dd3caf3ff7acb659f86aaaafd7fcf649
Adriano Buttigliero Volpato <adriano.b.volpato@gmail.com>
2024-01-24 11:27:29 -0300

    Phone book V1

--- part05-17_phone_book_v1/src/phone_book_v1.py
+++ part05-17_phone_book_v1/src/phone_book_v1.py
@@ -1,1 +1,28 @@
 # Write your solution here
+phone_book = {}
+
+
+while True:
+    action = int(input("command (1 search, 2 add, 3 quit): "))
+    if action == 1:
+        name = input("name: ")
+        if name not in phone_book:
+            print("no number")
+            continue
+        print(phone_book[name])
+        
+        
+        
+    if action == 2:
+        name = input("name: ")
+        phone = input("number: ")
+        phone_book[name] = phone
+        print("ok!")
+        
+        
+        
+    if action == 3:
+        print("quitting...")
+        break
+    
+    
