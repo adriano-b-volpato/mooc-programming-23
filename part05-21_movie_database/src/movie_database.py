1e8de232ce495cb33bbecf857157d11c12e11fad
Adriano Buttigliero Volpato <adriano.b.volpato@gmail.com>
2024-01-24 16:31:09 -0300

    Movie Database

--- part05-21_movie_database/src/movie_database.py
+++ part05-21_movie_database/src/movie_database.py
@@ -1,1 +1,49 @@
-# Write your solution here
+# # Write your solution here
+
+# Please write a function named add_movie(database: list, name: str, director: str, year: int, runtime: int), 
+# which adds a new movie object into a movie database.
+
+# The database is a list, and each movie object in the list is a dictionary. The dictionary should contain the following keys.
+
+#     name
+#     director
+#     year
+#     runtime
+
+# The values attached to these keys are given as arguments to the function.
+
+
+
+
+def add_movie(database: list, name: str, director: str, year: int, runtime: int):
+    movie = {}
+    movie["name"] = name
+    movie["director"] = director
+    movie["year"] = year
+    movie["runtime"] = runtime
+    database.append(movie)
+    
+        # movie = {"name": name,
+
+        #        "director": director,
+
+        #        "year": year,
+
+        #        "runtime": runtime}
+    
+    
+    
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    database = []
+    add_movie(database, "Gone with the Python", "Victor Pything", 2017, 116)
+    add_movie(database, "Pythons on a Plane", "Renny Pytholin", 2001, 94)
+    print(database)
