5f668b2a95732ab9a443d62b5eda6764327f556f
Adriano Buttigliero Volpato <adriano.b.volpato@gmail.com>
2024-01-25 17:46:01 -0300

    Create a Tuple

--- part05-23_create_tuple/src/create_tuple.py
+++ part05-23_create_tuple/src/create_tuple.py
@@ -1,1 +1,23 @@
 # Write your solution here
+
+
+def create_tuple(x: int, y: int, z: int):
+    
+    temp = [x, y, z]
+    sorted_temp = sorted(temp)
+    temp = []
+    temp.append(sorted_temp[0])
+    temp.append(sorted_temp[1])
+    temp.append(sorted_temp[2])
+    temp.append(sorted_temp[0] + sorted_temp[1] + sorted_temp[2])
+    coordinates = (temp[0], temp[2], temp[-1])
+    return coordinates
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print(create_tuple(1, 4, 2))
