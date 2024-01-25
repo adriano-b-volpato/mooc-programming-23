63c1791539303349f9ce1e26e21fed29174287d1
Adriano Buttigliero Volpato <adriano.b.volpato@gmail.com>
2024-01-25 17:54:28 -0300

    Oldest Person

--- part05-24_oldest_person/src/oldest_person.py
+++ part05-24_oldest_person/src/oldest_person.py
@@ -1,1 +1,36 @@
 # Write your solution here
+
+def oldest_person(people: list):
+    dates = []
+    
+    for person in people:
+        dates.append(person[1])
+    
+    oldest_year = dates.index(min(dates))
+    return people[oldest_year][0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    p1 = ("Adam", 1977)
+    p2 = ("Ellen", 1985)
+    p3 = ("Mary", 1953)
+    p4 = ("Ernest", 1997)
+    people = [p1, p2, p3, p4]
+
+    print(oldest_person(people))
