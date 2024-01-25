10de43897ca9468a9f5faaf9bcf9b84a9b4dc3e2
Adriano Buttigliero Volpato <adriano.b.volpato@gmail.com>
2024-01-25 20:07:26 -0300

    Student Database pt 1

--- part05-26_student_database/src/student_database.py
+++ part05-26_student_database/src/student_database.py
@@ -1,1 +1,25 @@
 # Write your solution here
+# students = {}
+
+def add_student(students : dict, student : str):
+    students[student] = [student]
+    return students
+
+
+
+def print_student(students : dict, student : str):
+    if student not in students:
+        print(f"{student}: no such person in the database")
+        return
+    print(f"{student}: \n no completed courses")
+     
+    
+    
+    
+if __name__ == "__main__":
+    students = {}
+    add_student(students, "Peter")
+    add_student(students, "Eliza")
+    print_student(students, "Peter")
+    print_student(students, "Eliza")
+    print_student(students, "Jack")
