fbdd0965d36fc59d0b1c5c843ff4c708805075c3
Adriano Buttigliero Volpato <adriano.b.volpato@gmail.com>
2024-01-26 14:16:53 -0300

    Student Database pt2

--- part05-26_student_database/src/student_database.py
+++ part05-26_student_database/src/student_database.py
@@ -1,25 +1,53 @@
 # Write your solution here
 # students = {}
-
 def add_student(students : dict, student : str):
-    students[student] = [student]
+    students[student] = {"name": student, "courses": []}
     return students
 
-
+  
+def add_course(students : dict, student : str, course : tuple):
+    students[student]["courses"].append(course)
+    
 
 def print_student(students : dict, student : str):
     if student not in students:
         print(f"{student}: no such person in the database")
         return
-    print(f"{student}: \n no completed courses")
-     
+    
+    courses = []
+    sum_of_marks = 0
+    for course in students[student]["courses"]:
+        courses.append(course)
+        sum_of_marks += course[1]
+    
+    if not courses:
+        print(f"{student}: \n no completed courses")
+        return
+    print(f"{student}: ")
+    print(f" {len(courses)} completed courses: ")
+    for course in courses:
+        avg = sum_of_marks / len(courses)
+        print(f"  {course[0]} {course[1]}")
+    print(f" average grade {avg} ")
     
     
+
     
 if __name__ == "__main__":
     students = {}
     add_student(students, "Peter")
-    add_student(students, "Eliza")
+    add_student(students, "Emily")
     print_student(students, "Peter")
-    print_student(students, "Eliza")
-    print_student(students, "Jack")
+    print_student(students, "Emily")
+    print_student(students, "Andy")
+    # students = {}
+    # add_student(students, "Peter")
+    # add_student(students, "Eliza")
+    # print_student(students, "Eliza")
+    # print_student(students, "Jack")
+    # add_student(students, "Peter")
+    # add_course(students, "Peter", ("Introduction to Programming", 3))
+    # add_course(students, "Peter", ("Advanced Course in Programming", 2))
+
+    # print_student(students, "Peter")
+    
